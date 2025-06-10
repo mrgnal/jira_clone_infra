141d1cbf365be9f5412d7abd9586f851dbdc94e5
Vitalii <alexvitalik65@gmail.com>
2025-06-10 08:48:59 +0300

    Demo1

--- splunk/scripts/bootstrap.sh
+++ splunk/scripts/bootstrap.sh
@@ -3,7 +3,6 @@ set -a
 source /home/vagrant/.env
 set +a
 
-
 echo "Update system"
 sudo apt update && sudo apt upgrade -y
 
@@ -12,3 +11,13 @@ sudo apt install -y wget
 
 echo "Downloading Splunk"
 wget -O $DEB_FILE "$SPLUNK_DEB_URL"
+
+echo "Installing Splunk"
+dpkg -i $DEB_FILE
+
+echo "Accepting license and starting Splunk"
+/opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt \
+  --seed-passwd "$SPLUNK_PASS"
+
+echo "Enabling Splunk on boot..."
+/opt/splunk/bin/splunk enable boot-start

486df03978d8b673c78182312e5efa5ef700e928
Vitalii <alexvitalik65@gmail.com>
2025-06-20 03:56:22 +0300

    Terrafor configuration for aws jenkins user

--- jenkins/scripts/master_setup.sh
+++ jenkins/scripts/master_setup.sh
@@ -4,14 +4,15 @@ source /home/vagrant/.env
 set +a
 
 echo "Update system"
-sudo apt update && sudo apt upgrade -y
+# sudo apt update && sudo apt upgrade -y
 
 echo "Install jdk"
 sudo sudo apt install -y default-jdk
 
 echo "Add jenkins repo"
-wget -q -O - "$JENKINS_KEY" | sudo apt-key add -
-echo "deb $JENKINS_REPO binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+wget -q -O - $JENKINS_KEY | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] $JENKINS_REPO binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+
 
 echo "Install jenkins"
 sudo apt install -y jenkins
